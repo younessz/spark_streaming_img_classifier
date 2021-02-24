e9abeab440a2100c126b7161b94e6073e7c5305e
Youness <younesszarhloul@gmail.com>
2021-02-24 14:52:38 +0100

    remove deprecated files

--- StreamingApp/build.sbt
+++
@@ -1,5 +0,0 @@
-name := "StreamingApp"
-
-version := "0.1"
-
-scalaVersion := "2.13.4"
--- StreamingApp/src/main/scala/ImagePipeline.scala
+++
@@ -1,7 +0,0 @@
-object ImagePipeline {
-
-  def main(args: Array[String]): Unit = {
-    println("Hello, world!")
-  }
-}
-
